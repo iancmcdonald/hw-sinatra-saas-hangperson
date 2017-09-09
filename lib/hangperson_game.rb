97a0d3600f39a9d448801c2e87c124ff7f602c55
Ian McDonald <ianmcdonald@Ians-MacBook-Pro-3.local>
2017-09-08 21:21:05 -0700

    finished game logic

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,57 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
   def initialize(word)
-    @word = word
+    if word.nil? || word =~ /.*[^A-Za-z].*/
+      raise ArgumentError
+    end
+
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
+  # @param [String] letter
+  def guess(letter)
+    if letter.nil? || letter.length != 1 || letter =~ /[^A-Za-z]/
+      raise ArgumentError
+    end
+
+    letter.downcase!
+
+    if guesses.include?(letter) || wrong_guesses.include?(letter)
+      return false
+    end
+
+    if word.include? letter
+      guesses << letter
+    else
+      wrong_guesses << letter
+    end
+    true
+  end
+
+  def word_with_guesses
+    if guesses.empty?
+      word.tr('a-z', '-')
+    else
+      word.tr('^' + guesses, '-')
+    end
+  end
+
+  def check_win_or_lose
+    if wrong_guesses.length == 7
+      :lose
+    elsif word_with_guesses == word
+      :win
+    else
+      :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -19,7 +67,7 @@ class HangpersonGame
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
-    Net::HTTP.new('watchout4snakes.com').start { |http|
+    Net::HTTP.new('watchout4snakes.com').start {|http|
       return http.post(uri, "").body
     }
   end
